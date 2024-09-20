1cb9fa2278f50d0b4146e1c1113d2827ef22f663
OleksandrKolko <144420883+OleksandrKolko@users.noreply.github.com>
2024-09-20 18:39:50 +0300

    Update ex_2.14.c

--- ex_2.14.c
+++ ex_2.14.c
@@ -3,6 +3,7 @@
 
 int main() {
     float a, b, c;
+    float eps = 0.0000001;
     printf("a = \n");
     scanf("%f", &a);
     printf("b = \n");
@@ -10,8 +11,17 @@ int main() {
     printf("c = \n");
     scanf("%f", &c);
     float dis = (b * b) - (4 * a * c);
-    float x1 = ((-1 * b) - sqrt(dis)) / (2 * a);
-    float x2 = ((-1 * b) + sqrt(dis)) / (2 * a);
-    printf("x1 = %.0f\n", x1);
-    printf("x2 = %.0f\n", x2);
+    if (dis < 0) {
+        return 0;
+    }
+    else if(fabs(dis) < eps){
+        float x = (-1 * b) / (2 * b);
+        printf("x = %.0f\n", x);
+    }
+    else {
+        float x1 = ((-1 * b) - sqrt(dis)) / (2 * a);
+        float x2 = ((-1 * b) + sqrt(dis)) / (2 * a);
+        printf("x1 = %.0f\n", x1);
+        printf("x2 = %.0f\n", x2);
+    }
 }
