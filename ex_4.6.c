0ef08db5bdc87914f775aa338c503f8464963635
OleksandrKolko <144420883+OleksandrKolko@users.noreply.github.com>
2024-09-29 22:11:53 +0300

    Update ex_4.6.c

--- ex_4.6.c
+++ ex_4.6.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <math.h>
-
-int main() {
-    unsigned int n, i;
+double task_a(unsigned int n) {
     double res = 0;
-    printf("n = \n");
-    scanf("%u", &n);
-
-    for (i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++) {
         res += sqrt(2 + res);
     }
-    printf("%lf", res);
+    return res;
+}
+
+double task_b(unsigned int n) {
+    unsigned int p;
+    double res = 0;
+    p = n;
+    for (int i = 1; i < n; i++) {
+        res += sqrt(3 * p);
+        p -= 1;
+    }
+    return res;
+}
+
+int main() {
+    unsigned int n;
+    printf("n = \n");
+    scanf("%u", &n);
+    double res = task_a(n);
+    printf("res = %lf\n", res);
+    double res_2 = task_b(n);
+    printf("res_2 = %lf", res_2);
 }
